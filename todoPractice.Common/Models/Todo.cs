e6d9fccac012274d41cecabac0f995113348461c
Esteban <estebanduque2@gmail.com>
2021-08-16 21:25:32 -0500

    Add entity

---
+++ todoPractice.Common/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace todoPractice.Common.Models
+{
+    internal class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ todoPractice.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace todoPractice.Functions.Entities
+{
+    internal class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
